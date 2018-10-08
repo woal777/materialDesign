fbc98d9213abb58334e1a00683065cea7efc6349
jinho-laptop <woal777@naver.com>
2018-10-08 19:22:25 +0900

    gulp

--- GULP/input/fromCif.py
+++ GULP/input/fromCif.py
@@ -1,6 +1,7 @@
 from pymatgen.command_line.gulp_caller import GulpIO, Structure
 
 gio = GulpIO()
+gio.specie_potential_lines()
 with open('gin', 'w') as f:
     f.write(
         gio.buckingham_input(Structure.from_file('hfo2-amor.cif'), ['conv', 'md'])
