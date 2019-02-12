3623dae1b5a370d1ca84593f68788efb910ce4bf
jinho93 <a94041404>
2019-02-12 15:29:58 +0900

    from cluster

--- VASP/vasprun_related/extract_dos.py
+++ VASP/vasprun_related/extract_dos.py
@@ -3,19 +3,29 @@ from pymatgen.io.vasp.outputs import CompleteDos, Dos, Spin, Vasprun
 import numpy as np
 from pymatgen.electronic_structure.plotter import DosPlotter
 import os
-os.chdir('/home/jinho93/oxides/amorphous/igzo/band')
+os.chdir('/home/jinho93/molecule/oep-sub_fe/110/cooep/1-mol/dos')
 vrun = Vasprun('vasprun.xml')
-#mpr = MPRester('DhmFQPuibZo8JtXn')
+# mpr = MPRester('DhmFQPuibZo8JtXn')
 dos = vrun.complete_dos
-#dos: CompleteDos = mpr.get_dos_by_material_id('mp-352')
+# dos: CompleteDos = mpr.get_dos_by_material_id('mp-352')
 eldos = dos.get_element_dos()
 dsp = DosPlotter()
 dsp.add_dos_dict(eldos)
-dsp.show(ylim=(-20, 5))
+dsp.show(xlim=(-20, 5))
 arr = list()
-arr.append(dos.energies - dos.efermi)
+if vrun.is_spin:
+    arr.append(np.concatenate((dos.energies - dos.efermi,
+                               np.flip(dos.energies) - dos.efermi)))
+else:
+    arr.append(dos.energies - dos.efermi)
 j: Dos
+items = []
 for i, j in eldos.items():
-    arr.append(j.densities[Spin.up])
+    items.append(str(i))
+    if vrun.is_spin:
+        arr.append(np.concatenate((j.densities[Spin.up] / max(j.get_densities(Spin.up)),
+                                   np.flip(-j.densities[Spin.down]) / max(j.get_densities(Spin.down)))))
+    else:
+        arr.append(j.densities[Spin.up] / max(j.get_densities(Spin.up)))
 arr = np.array(arr)
-np.savetxt('output.dat', arr.transpose(), fmt='%.9e', delimiter='    ')
+np.savetxt('output.dat', arr.transpose(), fmt='%.9e', delimiter='    ', header=' '.join(items))
