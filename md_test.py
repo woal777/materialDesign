bf8a5d58a415f92fc918546ad07503d45e0f46a6
jinho93 <a94041404>
2022-05-06 14:14:08 +0900

    master

--- md_test.py
+++
@@ -1,5 +0,0 @@
-from pymatgen import MPRester, Structure
-import sys
-mpr = MPRester('DhmFQPuibZo8JtXn')
-s: Structure = mpr.get_structure_by_material_id(sys.argv[1])
-s.to('POSCAR', 'POSCAR')
--- openmx_interface.py
+++
@@ -1,9 +0,0 @@
-import seekpath
-from pymatgen import Structure
-import os
-
-
-os.chdir('/home/ksrc5/FTJ/bfo/sto-bfo')
-s = Structure.from_file('POSCAR')
-skp = seekpath.get_explicit_k_path(s)
-print(skp)
